adde4813c39fb09e8af0fb790c2b0656e1689ff6
sebrinamusbah <sebrinm9@gmail.com>
2025-12-22 15:20:45 +0300

    build

---
+++ cart/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  total: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+      state.total += action.payload.price;
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.id !== action.payload.id);
+      state.total -= action.payload.price;
+    },
+    clearCart: (state) => {
+      state.items = [];
+      state.total = 0;
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
