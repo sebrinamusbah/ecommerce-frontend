3561126e3d601ef7ea37ad1c91fa1521085ade11
sebrinamusbah <sebrinm9@gmail.com>
2025-12-23 16:41:57 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Home.jsx";
+import HomePage from "./pages/Home";
 import ProductPage from "./pages/ProductDetails.jsx";
 import CartPage from "./pages/Cart.jsx";
 
