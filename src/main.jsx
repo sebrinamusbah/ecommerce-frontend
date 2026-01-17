4b9d6a05d3c30cb0a0356de81894fba4427d81af
sebrinamusbah <sebrinm9@gmail.com>
2026-01-17 19:51:47 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { AppProvider } from "./context/appContext.jsx"; // or "./context/AppContext.jsx"
 
-import { AppProvider } from "./context/appContext.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <AdminProvider>
-        <App />
-      </AdminProvider>
-    </AuthProvider>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </StrictMode>
 );
